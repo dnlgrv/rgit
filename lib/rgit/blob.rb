591f78b3c318d26be61da55667bdc7d75db2ec1c
Daniel Grieve <dnlgrv@hey.com>
2023-08-15 10:50:07 +0100

    Serialize blobs back to bytes

--- lib/rgit/blob.rb
+++ lib/rgit/blob.rb
@@ -8,6 +8,11 @@ class Rgit::Blob
   end
 
   def initialize(content)
+    @header = "blob #{content.bytesize}"
     @content = content
+  end
+
+  def serialize
+    [@header, "\0", @content].join.unpack("c*")
   end
 end
